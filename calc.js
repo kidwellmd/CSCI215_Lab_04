2a46bf50cfdb6af223c31a3e58a13a3881ef4321
kidwellmd <42657772+kidwellmd@users.noreply.github.com>
2018-12-11 18:02:05 -0500

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 // -----------------------------------
 // Global Variables Go Here
-	var I ;
+	var I = [] ;
 	I[0]="http://munsellb.people.cofc.edu/img/pound.jpeg";
 	I[1]="http://munsellb.people.cofc.edu/img/euro.jpeg";
 	I[2]="'http://munsellb.people.cofc.edu/img/kroon.jpeg";
@@ -12,39 +12,38 @@
 
 // -----------------------------------
 // Convert Function Goes Here
-function convert {
+function convert () {
  var dollar = document.getElementsByName("dollars")[0].value;
- var selectElement = document.getElementById('currency');
- var exhangeRate= selectElement.value;
- var index= selectElement.selectedIndex;
- var currency= selectElement.options[index].text;
- 
- updateExchange( dollar, exchangeRate, currency, index );
+        var selectElement = document.getElementById('currency');
+        var exchangeRate = selectElement.value;
+        var index = selectElement.selectedIndex;
+        var currency = selectElement.options[index].text;
+        updateExchange( dollar, exchangeRate, currency, index )
+		
  
 }
  
 // -----------------------------------
 // Calculate Function Goes Here
-function calculate (dollar , exchangeRate){
-	
-	return:dollar*exchangeRate
-};
+ function calculate(dollar, exchangeRate) {
+        product = dollar * exchangeRate;
+        return product;
+}
 
- Console.out(dollar*exchangeRate);
 // -----------------------------------
 // Functions that are given
 // Do not modify
 
 function updateImage( index ) { 
     
-    var url = 'url(' + I[index] + ')';
+        var url = 'url(' + I[index] + ')';
     document.getElementById('bill_img').style.backgroundImage=url;
     
 }
 
 function updateExchange( dollar, exchangeRate, currency, index ) {
     
-    document.getElementsByName("exchange")[0].value = calculate( dollar, exchangeRate ).toPrecision(4) + ' ' + currency;
+     document.getElementsByName("exchange")[0].value = calculate( dollar, exchangeRate ).toPrecision(4) + ' ' + currency;
     
     updateImage( index );
     
